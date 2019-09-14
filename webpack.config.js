5039c2831a3dbdf1573be3508ae7682ae3f7556e
Daniel H. Guissepe <ing.dhguissepe@gmail.com>
2019-09-14 14:21:52 -0500

    defining new enviroment variable

--- webpack.config.js
+++ webpack.config.js
@@ -92,6 +92,12 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
       'process.env.PORT': JSON.stringify(process.env.PORT)
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize : true,
+      compress : {
+          warnings : false
+      }
     })
   ]
 }

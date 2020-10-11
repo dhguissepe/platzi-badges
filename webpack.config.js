d5446116e9f180554f2aa0e653d6227a73fb6d52
Daniel H. Guissepe <daniel.hernandez@marfeel.com>
2020-10-11 13:29:49 -0500

    Add favicon.

--- webpack.config.js
+++ webpack.config.js
@@ -69,8 +69,9 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public/index.html')
-      }),
+      template: path.resolve(__dirname, 'public/index.html'),
+      favicon: path.resolve(__dirname, 'public', 'favicon.ico'),
+    }),
     new webpack.DllReferencePlugin({
       manifest: require('./modules-manifest.json')
     }),
